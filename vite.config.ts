c7f1d0c6bec15ff0ddbe39dcd24963aeadd05fc5
HoppeDevz <hoppecsgonet@hotmail.com>
2024-02-23 16:58:07 -0300

    changed preview port

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   preview: {
-    port: 8080,
+    port: 80,
   },
   plugins: [react()],
 })
